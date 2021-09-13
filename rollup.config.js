fea3b32f28a1ca60a922951070827f6e1d24635f
Immanuel Bauer <gimmix.at@gmail.com>
2021-09-13 22:06:42 +0200

    added minification to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,36 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import scss from 'rollup-plugin-scss';
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: 'src/index.ts',
     output: [{
         file: 'dist/bundle.js',
         name: 'UIGauges',
-        format: 'iife'
+        format: 'umd'
     },{
-        file: 'dist/bundle.cjs.js',
-        format: 'cjs'
+        file: 'dist/bundle.min.js',
+        name: 'UIGauges',
+        format: 'umd',
+        plugins: [terser()],
+        sourcemap: true,
+    },{
+        file: 'dist/bundle.esm.js',
+        format: 'esm'
+    },{
+        file: 'dist/bundle.esm.min.js',
+        format: 'esm',
+        plugins: [terser()],
+        sourcemap: true,
     }],
     plugins: [
         nodeResolve(),
         typescript(),
         scss({
-            output: 'dist/bundle.css'
+            output: 'dist/bundle.min.css',
+            outputStyle: 'compressed',
+            sourceMap: true,
         }),
     ]
 };
